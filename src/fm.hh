ee98921fe7c8a80d7f4ce91d2e471e6c34da10db
Julius Ikkala <julius.ikkala@gmail.com>
2019-01-08 18:53:58 +0200

    Add license

--- src/fm.hh
+++ src/fm.hh
@@ -1,3 +1,21 @@
+/*
+    Copyright 2018-2019 Julius Ikkala
+
+    This file is part of CafeFM.
+
+    CafeFM is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    CafeFM is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with CafeFM.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #ifndef CAFEFM_FM_HH
 #define CAFEFM_FM_HH
 #include "func.hh"
